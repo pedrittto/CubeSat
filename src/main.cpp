ef47611c40420b842a9311f41bdaa37485b7910c
Piotr Radziej <piotrekradziej0@gmail.com>
2026-01-18 19:25:36 +0100

    Traffic Light time-based sequence

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,24 @@
-#include <Arduino.h> 
+#include <Arduino.h>
 
 const int PIN_RED=12;
-const int PIN_YELLOW=14;
 const int PIN_GREEN=27;
+const int PIN_YELLOW=14;
 
 const int STAN_RED=1;
 const int STAN_YELLOW=2;
 const int STAN_GREEN=3;
 
 int aktualnyStan=STAN_RED;
-int czasOstatniejZmiany=0;
+
+unsigned long czasOstatniejZmiany=0;
 
 const int CZAS_RED=1000;
-const int CZAS_YELLOW=1000;
-const int CZAS_GREEN=2000;
+const int CZAS_YELLOW=500;
+const int CZAS_GREEN=1000;
 
-void setup(){
+void setup (){
   Serial.begin(115200);
+
   digitalWrite(PIN_RED,HIGH);
   digitalWrite(PIN_YELLOW,LOW);
   digitalWrite(PIN_GREEN,LOW);
@@ -31,11 +33,12 @@ void loop(){
   unsigned long aktualnyCzas=millis();
 
   switch (aktualnyStan){
+
     case STAN_RED:
     if (aktualnyCzas-czasOstatniejZmiany>=CZAS_RED){
       aktualnyStan=STAN_YELLOW;
       czasOstatniejZmiany=aktualnyCzas;
-      digitalWrite(PIN_RED,LOW);
+      digitalWrite(PIN_RED,HIGH);
       digitalWrite(PIN_YELLOW,HIGH);
     }
     break;
@@ -44,20 +47,20 @@ void loop(){
     if (aktualnyCzas-czasOstatniejZmiany>=CZAS_YELLOW){
       aktualnyStan=STAN_GREEN;
       czasOstatniejZmiany=aktualnyCzas;
-
-    digitalWrite(PIN_YELLOW,LOW);
-    digitalWrite(PIN_GREEN,HIGH);
-    }
+      digitalWrite(PIN_YELLOW,LOW);
+      digitalWrite(PIN_RED,LOW);
+      digitalWrite(PIN_GREEN,HIGH);
+    } 
     break;
 
     case STAN_GREEN:
-    if(aktualnyCzas-czasOstatniejZmiany>=CZAS_GREEN){
+    if (aktualnyCzas-czasOstatniejZmiany>=CZAS_GREEN){
       aktualnyStan=STAN_RED;
       czasOstatniejZmiany=aktualnyCzas;
       digitalWrite(PIN_GREEN,LOW);
       digitalWrite(PIN_RED,HIGH);
+  }
+  break ;
 
-    }
-    break;
   }
 }
